267595edbd3b2eaaf2ce41d6725ba2726e94a2d1
Raphael Meyer <raphael.akira.meyer@gmail.com>
2026-02-26 07:22:49 +0100

    second temperature sensor

--- src/system_control.adb
+++ src/system_control.adb
@@ -2,8 +2,10 @@ with Sensors;
 
 procedure System_Control is
    T8 : constant Sensors.Sensor := (Id => 808, Temperature_Value => <>);
+   J6 : constant Sensors.Sensor := (Id => 160, Temperature_Value => <>);
 begin
    loop
       Sensors.Display (T8);
+      Sensors.Display (J6);
    end loop;
 end System_Control;
